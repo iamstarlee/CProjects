ede5efca903fae59e4a62a990b5f4bccca49589e
iamstarlee <13253620092@163.com>
2021-10-28 09:57:43 +0800

    second commit

--- shiyan1.cpp
+++ shiyan1.cpp
@@ -6,33 +6,61 @@ class Point
     private:
         int x, y;
     public:
+        Point();
+        Point(Point &p);
         Point(int a, int b);
         ~Point();
+        void Display() const;
 };
 Point::Point(int a, int b)
 {
     x = a;
     y = b;
 }
+Point::Point(Point &p){
+    x = p.x;
+    y = p.y;
+}
 Point::~Point()
 {
     cout<<"Point's Destructor called"<<endl;
 }
+void Point::Display() const
+{
+    cout<<"my x-axis is "<<x<<" and my y-axis is "<<y<<endl;
+}
+
 
 class Line: public Point
 {
     private:
-        int x, y;
+        Point p;
+        
     public:
-        Line(int a, int b, int c, int d);
+        Line(Point p1, Point p2);
         ~Line();
+        void Display() const;
 };
-Line::Line(int a, int b, int c, int d):Point(a, b)
+
+Line::Line(Point p1, Point p2):Point(p2)
 {
-    x = c;
-    y = d;
+    p = p1;
 }
 Line::~Line()
 {
     cout<<"Line's Destructor called"<<endl;
+}
+void Line::Display() const
+{
+    Point::Display();
+    p.Display();
+}
+
+
+int main()
+{
+    Point obj1(1, 2), obj2(3, 4);
+    Line line1(obj1, obj2);
+    line1.Display();
+    
 }
