7bfefaf217bc33a07a4a33b485406c8ac228a23f
iamstarlee <13253620092@163.com>
2021-11-01 22:25:53 +0800

    today is 2021-11-1

--- shiyan1.cpp
+++ shiyan1.cpp
@@ -38,12 +38,17 @@ class Line: public Point
         
     public:
         Line(Point p1, Point p2);
+        Line(Line &line1);
         ~Line();
         void Display() const;
 };
 Line::Line(Point p1, Point p2):Point(p2), p(p1)
 {
     cout<<"Line's Constructor called"<<endl;
+}
+Line::Line(Line & line1)
+{
+    p = line1.p;
 }
 Line::~Line()
 {
@@ -62,18 +67,73 @@ class Circle:public Point
         
     public:
         Circle(Point p1, int r);
-        void Display();
+        ~Circle();
+        void Display() const;
 };
 Circle::Circle(Point p1, int r):Point(p1)
 {
     radius = r;
     cout<<"Circle's Constructor called"<<endl;
 }
-void Circle::Display()
+Circle::~Circle()
+{
+    cout<<"Circle's Destructor called"<<endl;
+}
+void Circle::Display() const
 {
     // cout<<"The center of the circle is "<<endl;
     Point::Display();
     cout<<"The radius is "<<radius<<endl;
+}
+
+
+class Rectangle:public Line
+{
+    private:
+        Line line1;
+    public:
+        Rectangle(Line line1, Line line2);
+        // Rectangle(Line &line1);
+        ~Rectangle();
+        void Display() const;
+
+};
+Rectangle::Rectangle(Line l1, Line l2):line1(l1), Line(l2)
+{
+    cout<<"Line's Constructor called"<<endl;
+}
+Rectangle::~Rectangle()
+{
+    cout<<"Rectangle's Desctructor called"<<endl;
+}
+void Rectangle::Display() const
+{
+    line1.Display();
+}
+
+
+class Curve
+{
+    private:
+        Point p1, p2;
+    public:
+        Curve(Point p1, Point p2);
+        ~Curve();
+        void Display() const;
+};
+Curve::Curve(Point p1, Point p2)
+{
+    this->p1 = p1;
+    this->p2 = p2;
+}
+Curve::~Curve()
+{
+    cout<<"Curve's Dectructor called"<<endl;
+}
+void Curve::Display() const
+{
+    p1.Display();
+    p2.Display();
 }
 
 
